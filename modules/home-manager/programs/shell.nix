06ecb312788af58acdd295145beb57922e36d56f
Noah Syrkis <noah@syrkis.com>
2025-06-10 22:25:34 +0200

    zoxide

--- modules/home-manager/programs/shell.nix
+++ modules/home-manager/programs/shell.nix
@@ -2,20 +2,34 @@
   programs = {
     # Modern shell prompt
     starship.enable = true;
-    
+
     # Terminal multiplexer
     zellij.enable = true;
-    
+
     # Better cat with syntax highlighting
     bat.enable = true;
-    
+
     # Directory navigation
     zoxide = {
       enable = true;
       enableFishIntegration = true;
       enableZshIntegration = true;
+      # This creates the 'z' command, but we want 'cd' to use zoxide too
     };
-    
+
+    # Shell aliases
+    bash.shellAliases = {
+      cd = "z"; # Replace cd with zoxide
+    };
+
+    zsh.shellAliases = {
+      cd = "z"; # Replace cd with zoxide
+    };
+
+    fish.shellAliases = {
+      cd = "z"; # Replace cd with zoxide
+    };
+
     # Modern ls replacement
     eza = {
       enable = true;
@@ -24,14 +38,14 @@
       git = true;
       icons = "auto";
     };
-    
+
     # Fuzzy finder
     fzf = {
       enable = true;
       enableFishIntegration = true;
       enableZshIntegration = true;
     };
-    
+
     # Better grep
     ripgrep.enable = true;
   };
